ed0464ceb6bd4298891de0273d5c3456ef0d6112
Alexander Vu <alexander.vu.tuyet@gmail.com>
2020-01-22 22:25:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server');
+const schema = require('./lib/schema')
+const dataSources = require('./lib/datasources')
+const context = require('./lib/context')
+
+
+// Create ApolloServer instance by
+// adding schema, dataSources, context informations.
+const server = new ApolloServer({
+  schema,
+  dataSources,
+  context
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
